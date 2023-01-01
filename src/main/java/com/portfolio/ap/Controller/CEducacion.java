b4f3d7c25fd4ff57303729a4c91f2e19cb831719
GonzaloMertes <gonzaaameeerteees@gmail.com>
2023-01-01 12:59:25 -0300

    Update CEducacion.java

--- src/main/java/com/portfolio/ap/Controller/CEducacion.java
+++ src/main/java/com/portfolio/ap/Controller/CEducacion.java
@@ -22,19 +22,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/edu")
-//@CrossOrigin(origins = "http://localhost:4200")
+//@RequestMapping("/edu")
 @CrossOrigin(origins = "https://frontendprueba-43463.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
     
-    @GetMapping("/lista")
+    @GetMapping("/edu/lista")
     public ResponseEntity<List<Educacion>> list(){
         List<Educacion> list = sEducacion.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
-    @GetMapping("/detail/{id}")
+    @GetMapping("/edu/detail/{id}")
     public ResponseEntity<Educacion> getById(@PathVariable("id")int id){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
@@ -44,7 +43,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/edu/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
@@ -53,7 +52,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educacion eliminada"), HttpStatus.OK);
     }
     
-    @PostMapping("/create")
+    @PostMapping("/edu/create")
     public ResponseEntity<?> create(@RequestBody dtoEducacion dtoeducacion){
         if(StringUtils.isBlank(dtoeducacion.getNombreE())){
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
@@ -70,7 +69,7 @@ public class CEducacion {
                 
     }
     
-    @PutMapping("/update/{id}")
+    @PutMapping("/edu/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducacion){
         if(!sEducacion.existsById(id)){
             return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
