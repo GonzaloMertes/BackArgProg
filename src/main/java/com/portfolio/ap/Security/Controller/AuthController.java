999a0dcafea686a9704ab54aa7d0a2be4c60286d
GonzaloMertes <gonzaaameeerteees@gmail.com>
2023-01-01 12:25:50 -0300

    Update AuthController.java
    correccion 4

--- src/main/java/com/portfolio/ap/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ap/Security/Controller/AuthController.java
@@ -31,7 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping ("/auth")
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://frontendprueba-43463.web.app")
 public class AuthController {
    @Autowired
