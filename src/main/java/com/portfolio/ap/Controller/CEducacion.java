ed36b4c10156a6f2305058812b1e698d3e5679b8
GonzaloMertes <gonzaaameeerteees@gmail.com>
2023-01-01 10:57:44 -0300

    prueba 1 back

--- src/main/java/com/portfolio/ap/Controller/CEducacion.java
+++ src/main/java/com/portfolio/ap/Controller/CEducacion.java
@@ -23,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/edu")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendprueba-43463.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
--- src/main/java/com/portfolio/ap/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/ap/Controller/CExperiencia.java
@@ -22,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping ("explab")
-@CrossOrigin (origins = "http://localhost:4200")
+//@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendprueba-43463.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
