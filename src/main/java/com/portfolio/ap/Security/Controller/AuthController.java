e3ea467f182cf92888875467a38fc51e8401c89a
GonzaloMertes <gonzaaameeerteees@gmail.com>
2023-01-02 16:23:50 -0300

    11

--- src/main/java/com/portfolio/ap/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/ap/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontendprueba-43463.web.app")
+@CrossOrigin(origins = "**")
 
 public class AuthController {
    @Autowired
@@ -73,8 +73,8 @@ public class AuthController {
        
        return new ResponseEntity(new Mensaje("Usuario guardado"),HttpStatus.CREATED);
    }
-   @RequestMapping("/auth")
-   @CrossOrigin(origins = "https://frontendprueba-43463.web.app")
+  
+   
    @PostMapping("/login")
    public ResponseEntity<JwtDto> login(@Valid @RequestBody LoginUsuario loginUsuario,BindingResult bindingResult){
        if(bindingResult.hasErrors())
